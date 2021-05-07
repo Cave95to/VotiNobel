c428b0d4901368c432e81beeedc4b964880d3661
andre <andre@DESKTOP-PMTSH1N.lan>
2021-05-07 21:02:09 +0200

    miglioramento con list di cerca, che ha N casi per ogni livello

--- src/main/java/it/polito/tdp/nobel/model/Model.java
+++ src/main/java/it/polito/tdp/nobel/model/Model.java
@@ -1,5 +1,6 @@
 package it.polito.tdp.nobel.model;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -9,7 +10,7 @@ import it.polito.tdp.nobel.db.EsameDAO;
 public class Model {
 
 	private List<Esame> partenza;
-	private Set<Esame> soluzioneMigliore;
+	private List<Esame> soluzioneMigliore;
 	private double mediaSoluzioneMigliore;
 	
 	public Model() {
@@ -17,13 +18,14 @@ public class Model {
 		this.partenza = dao.getTuttiEsami();
 	}
 	
-	public Set<Esame> calcolaSottoinsiemeEsami(int numeroCrediti) {
+	public List<Esame> calcolaSottoinsiemeEsami(int numeroCrediti) {
 		
-		Set<Esame> parziale = new HashSet<>();
+		//Set<Esame> parziale = new HashSet<>();
+		List<Esame> parziale = new ArrayList<>();
 		
 		// conviene qui resettare soluzione migliore e la media, ogni volta che clicchiamo il bottone
 		this.mediaSoluzioneMigliore = 0;
-		this.soluzioneMigliore = new HashSet<>();
+		this.soluzioneMigliore = new ArrayList<>();
 		
 		
 		// funzione ricorsiva
@@ -38,7 +40,7 @@ public class Model {
 
 	// METODO STUPIDO. N! COMPLESSITA, NON CONVERGE CON N GRANDE
 	// abbiamo N casi al livello 0, N-1 al livelo 1 ecc
-	private void cerca(Set<Esame> parziale, int L, int m) {
+	private void cerca(List<Esame> parziale, int L, int m) {
 		
 		// CASI TERMINALI
 		
@@ -58,7 +60,7 @@ public class Model {
 					// la media di parziale è più alta di quella trovata, aggiorno la soluzione
 			if(media > this.mediaSoluzioneMigliore) {
 						// IMPORTANTE CREARE NUOVO OGGETTO SE NO COPIA SOLO IL RIFERIMENTO!!!
-				soluzioneMigliore = new HashSet<>(parziale);
+				soluzioneMigliore = new ArrayList<>(parziale);
 				this.mediaSoluzioneMigliore = media;
 			}
 			
@@ -85,7 +87,7 @@ public class Model {
 		}
 		*/
 		
-		// 2 metodo migliore ma non ci salva da tutti i problemi... aggiungiamo controllo i >= L
+		/* 2 metodo migliore ma non ci salva da tutti i problemi... aggiungiamo controllo i >= L
 		for(int i = 0; i < partenza.size(); i ++) {
 			
 			if(!parziale.contains(partenza.get(i)) && i >= L) {
@@ -94,11 +96,27 @@ public class Model {
 				parziale.remove(partenza.get(i));
 			}
 		}
+		 */
+		
+		// 3 metodo migliore che ci evita ripetizioni e fa convergere il problema
+		// NECESSARIO TRASFORMARE I SET IN LISTE PER FARE GET!!!
+				int lastIndex = 0;
+				if(parziale.size() > 0)
+					lastIndex = this.partenza.indexOf(parziale.get(parziale.size()-1));
+				
+				for(int i = lastIndex; i< partenza.size(); i ++) {
+					if(!parziale.contains(partenza.get(i))) {
+						parziale.add(partenza.get(i));
+						this.cerca(parziale, L+1, m);
+						parziale.remove(partenza.get(i));
+					}
+				}
+						
 		
 		
 	}
 
-	// METODO INTELLIGENTE. 2^N COMPLESSITA'
+	/* METODO INTELLIGENTE. 2^N COMPLESSITA'
 	private void cerca2(Set<Esame> parziale, int L, int m) {
 			
 		// CASI TERMINALI
@@ -144,8 +162,9 @@ public class Model {
 				parziale.remove(this.partenza.get(L));
 				this.cerca2(parziale, L+1, m);
 	}
+	*/
 	
-	public double calcolaMedia(Set<Esame> esami) {
+	public double calcolaMedia(List<Esame> esami) {
 		
 		int crediti = 0;
 		int somma = 0;
@@ -158,7 +177,7 @@ public class Model {
 		return somma/crediti;
 	}
 	
-	public int sommaCrediti(Set<Esame> esami) {
+	public int sommaCrediti(List<Esame> esami) {
 		int somma = 0;
 		
 		for(Esame e : esami)
